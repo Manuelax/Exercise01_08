b9f92ac2513ed89b12ceb0cdc9d5f864cf49afbd
Ange Kamgaing <akamgaingnecdem@algomau.ca>
2025-09-25 17:18:05 -0400

    Update PerfectSquare.java

--- PerfectSquare.java
+++ PerfectSquare.java
@@ -1,19 +1,20 @@
 public class PerfectSquare {
-	
-	public static void main(String[] args) {
-		System.out.println(isPerfectSquare(1));
-		System.out.println(isPerfectSquare(4));
-		System.out.println(isPerfectSquare(Integer.MAX_VALUE/100));
-		System.out.println(isPerfectSquare(255));
-		
-	}
-	
-	public static boolean isPerfectSquare(int num) {
-        for(int i = 1; i < num; i++) {
-        	if(i*i = num) 
-        		return true;
-        	else if (i*i > num) return false;
-        }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(1));                     // true
+        System.out.println(isPerfectSquare(4));                     // true
+        System.out.println(isPerfectSquare(Integer.MAX_VALUE / 100)); 
+        System.out.println(isPerfectSquare(255));                   // false
     }
 
+    public static boolean isPerfectSquare(int num) {
+        for (int i = 1; i * i <= num; i++) { // changed loop condition to 'i * i <= num' to handle all numbers correctly,
+            if (i * i == num) {  // Fixed comparison from '=' to '==' for checking equality,
+                return true;
+            } else if (i * i > num) {  return false;
+            }
+        }
+        return false; //  added 'return false' at the end to ensure the method always returns a boolean.
+    }
 }
+
